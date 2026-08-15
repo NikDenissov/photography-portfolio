565a4a06fc749dfb67d9277cc799cf1e5a97dc60
Nik Denissov <nik.denissov@gmail.com>
2026-08-15 20:02:20 +0100

    Created grid layout for the home/projects page

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,8 +1,22 @@
+import "./ProjectsPage.css";
+
+// Example photos
+const photos = import.meta.glob("../assets/photos/*.{jpg,jpeg,png,webp,JPG}",
+    {eager: true, query: "?url", import: "default",});
+
 export function ProjectsPage() {
+    const photoHrefs = Object.values(photos);
+
     return (
-        <>
-            <h2>Projects page</h2>
-            <p>Displays a list of all projects in a grid.</p>
-        </>
+        <div className = "projects-grid">
+            {
+                photoHrefs.map((photoHref) => (
+                    <div className = "project-card">
+                        <img key={photoHref} src={photoHref} alt=""/>
+                        <p>Project Name</p>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
