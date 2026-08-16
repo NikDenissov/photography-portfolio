e9406c3ee53f86ca21bab80139697d244718a3a9
Nik Denissov <nik.denissov@gmail.com>
2026-08-16 21:21:11 +0100

    Added styling and example content for the <project> page

--- src/pages/ProjectDetailsPage.tsx
+++ src/pages/ProjectDetailsPage.tsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom";
+import "./ProjectDetailsPage.css";
+
+// Example photos
+const photos = import.meta.glob("../assets/photos-various-sizes/*.{jpg,jpeg,png,webp,JPG}",
+    {eager: true, query: "?url", import: "default",});
 
 export function ProjectDetailsPage() {
+    const photoHrefs = Object.values(photos);
     const { slug } = useParams();
 
     return (
         <>
-            <h2>Project page</h2>
-            <p>Contains details about the selected project: {slug}</p>
+            <div className="project-description">
+                <h2>{slug}</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis eros arcu, nec iaculis neque hendrerit id. Aliquam erat volutpat. Proin vestibulum scelerisque nibh sed molestie. Fusce luctus maximus luctus. Nam placerat consequat dignissim. Integer eget felis id lorem luctus ullamcorper. Integer eu luctus massa. Sed scelerisque libero et lorem elementum congue. Vestibulum ut magna sed magna dapibus condimentum in sed eros.</p>
+            </div>
+            <div className = "project-photos-container">
+                {
+                    photoHrefs.map((photoHref) => (
+                        <img key={photoHref} src={photoHref} alt=""/>
+                    ))
+                }
+            </div>
         </>
     )
 }
